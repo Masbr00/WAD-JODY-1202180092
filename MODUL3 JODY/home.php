b610d726d203ebeeb1d89d83883565f8b4716188
Masbr00 <jodymardika@gmail.com>
2020-11-14 16:00:18 +0700

    home display

--- MODUL3 JODY/home.php
+++ MODUL3 JODY/home.php
@@ -3,9 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    <script type="text/javascript" src="assets/js/jquery.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.js"></script>
     <title>Home</title>
 </head>
 <body>
-    
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="#">EAD EVENTS</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse mx-auto" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active mr-3">
+                    <a class="nav-link" href="home.php">Home</a>
+                </li>
+                <li class="nav-item active">
+                    <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Buat Event</button>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!-- end Navbar -->
+
+    <!-- content -->
+    <div class="container mt-3">
+        <h3 class="text-primary" style="text-align: center;">WELCOME TO EAD EVENTS!<h3>
+    </div>
+    <!-- end of content -->
 </body>
 </html>
