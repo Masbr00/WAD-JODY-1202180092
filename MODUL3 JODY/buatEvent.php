8b820d19d7c37d71d7ac2a208f943b4a3c1e1997
Masbr00 <jodymardika@gmail.com>
2020-11-14 23:26:58 +0700

    navbar buat event

--- MODUL3 JODY/buatEvent.php
+++ MODUL3 JODY/buatEvent.php
@@ -9,25 +9,31 @@
     <title>Buat Event</title>
 </head>
 <body>
-        <!-- navbar -->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a class="navbar-brand" href="#">EAD EVENTS</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse mx-auto" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active mr-3">
-                        <a class="nav-link" href="home.php">Home</a>
-                    </li>
-                    <li class="nav-item active">
-                        <form action="buatevent.php">
-                            <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Buat Event</button>
-                        </form>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="#">EAD EVENTS</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse mx-auto" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active mr-3">
+                    <a class="nav-link" href="home.php">Home</a>
+                </li>
+                <li class="nav-item active">
+                    <form action="buatevent.php">
+                        <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Buat Event</button>
+                    </form>
+                </li>
+            </ul>
+        </div>
+    </nav>
     <!-- end Navbar -->
+
+    <!-- content -->
+    <div class="container-fluid mt-3 px-5">
+        <p class="text-primary px-2" style="font-size: 24px;">Buat Event
+    </div>
+    <!-- end of content -->
 </body>
 </html>
